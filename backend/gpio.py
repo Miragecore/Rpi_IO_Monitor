cbcb6ae4901d5c6bc9789f377a34d05a0a3fc72e
Miragecore <MirageKimYs@Gmail.com>
2022-07-06 08:22:39 +0900

    Add gpio test

--- backend/gpio.py
+++ backend/gpio.py
@@ -1,4 +1,15 @@
 from gpiozero import Button
+import logging
+from time import sleep
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("io.log"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
 
 def machine_IO_On():
     logging.info("IO On")
@@ -7,7 +18,14 @@ def machine_IO_Off():
     logging.info("IO Off")
 
 button = Button(21)
+led = LED(20)
 
-button.when_pressed = machine_IO_on
+button.when_pressed = machine_IO_On
 button.when_released = machine_IO_Off
 
+while True:
+    led.on();
+    sleep(2)
+    led.off()
+    slee(2)
+    
